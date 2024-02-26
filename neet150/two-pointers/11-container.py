3f9ee6d3499836b98510c8534d15d4ea90b57240
tonexie <tone.xie@gmail.com>
2024-02-26 14:37:27 +1300

    42

--- neet150/two-pointers/11-container.py
+++ neet150/two-pointers/11-container.py
@@ -1,3 +1,34 @@
+# best solution neet and mine
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        l, r = 0, len(height) - 1
+
+        maxVol = (r - l) * min(height[l], height[r])
+        maxL, maxR = height[0], height[-1]
+        while l < r:
+            if maxL <= maxR:
+                l += 1
+                if height[l] <= maxL:
+                    continue
+                else:
+                    maxL = height[l]
+            else:
+                r -= 1
+                if height[r] <= maxR:
+                    continue
+                else:
+                    maxR = height[r]
+
+            maxVol = max(maxVol, (r - l) * min(height[l], height[r]))
+
+        return maxVol
+
+
 class Solution(object):
     def maxArea(self, height):
         """
@@ -31,6 +62,22 @@ class Solution(object):
             maxVol = max(maxVol, (r - l) * min(height[l], height[r]))
 
         return maxVol
+
+
+# neet
+class Solution:
+    def maxArea(self, height):
+        l, r = 0, len(height) - 1
+        res = 0
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+
+        return res
 
 
 solution = Solution()
